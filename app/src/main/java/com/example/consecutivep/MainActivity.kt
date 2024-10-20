691aeb4465442178cfadb7e1cd33d3a913928191
SfBalaba <sf.balaba0170@gmail.com>
2024-10-21 01:42:02 +0500

    task3

--- app/src/main/java/com/example/consecutivep/MainActivity.kt
+++ app/src/main/java/com/example/consecutivep/MainActivity.kt
@@ -4,44 +4,108 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.navigation.compose.rememberNavController
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.consecutivep.ui.theme.ConsecutivePracticesTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.NavController
+import com.example.consecutivepracts.screens.HomeScreen
+import com.example.consecutivepracts.screens.MovieDetailScreen
+import com.example.consecutivepracts.screens.MovieListScreen
+import com.example.consecutivepracts.screens.SettingsScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ConsecutivePracticesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+                MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen() {
+    val navController = rememberNavController()
+    var currentDestination by remember { mutableStateOf("movies") }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ConsecutivePracticesTheme {
-        Greeting("Android")
+    Scaffold(
+        bottomBar = {
+            if (!currentDestination.startsWith("movie_detail")) {
+                BottomNavigationBar(navController = navController, currentDestination = currentDestination) {
+                    currentDestination = it
+                }
+            }
+        }
+    ) { innerPadding ->
+        NavHost(navController = navController, startDestination = "home", Modifier.padding(innerPadding)) {
+            composable("movies") {
+                currentDestination = "movies"
+                MovieListScreen { movieId ->
+                    navController.navigate("movie_detail/$movieId") {
+                    }
+                }
+            }
+            composable("movie_detail/{movieId}") { backStackEntry ->
+                val movieId = backStackEntry.arguments?.getString("movieId")?.toInt() ?: 0
+                currentDestination = "movie_detail"
+                MovieDetailScreen(movieId, navController)
+            }
+
+            composable("home") {
+                currentDestination = "home"
+                HomeScreen()
+            }
+            composable("settings") {
+                currentDestination = "settings"
+                SettingsScreen()
+            }
+
+        }
     }
 }
+
+@Composable
+fun BottomNavigationBar(navController: NavController, currentDestination: String, onDestinationChanged: (String) -> Unit) {
+    BottomNavigation(
+        backgroundColor = Color(0xFFD3D3D3),
+        contentColor = Color(0xFF000000)
+    ) {
+        val items = listOf(
+            BottomNavItem("Home", "home", R.drawable.home),
+            BottomNavItem("Movies", "movies", R.drawable.list),
+            BottomNavItem("Settings", "settings", R.drawable.settings)
+        )
+
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = { Icon(painter = painterResource(id = item.iconResId), contentDescription = item.title) },
+                label = { Text(item.title) },
+                selected = currentDestination == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+                        popUpTo(navController.graph.startDestinationId) { saveState = true }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                    onDestinationChanged(item.route)
+                }
+            )
+        }
+    }
+}
+
+data class BottomNavItem(val title: String, val route: String, val iconResId: Int)
---
+++ app/src/main/java/com/example/consecutivep/screens/SettingsScreen.kt
@@ -0,0 +1,14 @@
+package com.example.consecutivepracts.screens
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SettingsScreen() {
+    Text(text = "Settings Screen", modifier = Modifier.padding(16.dp))
+}
+
+
